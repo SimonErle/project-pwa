25fbcb619f45025b8e58b3a4e14801f9960fca38
Simone <simoneerle@gmail.com>
2018-07-02 19:01:33 +0200

    correction

--- src/components/app-details.js
+++ src/components/app-details.js
@@ -4,15 +4,15 @@ import './app-details.css';
 class AppDetails extends Component{
   render() {
     let company;
-    if ((this.props.company != null) && (this.props.company != ''))  {
+    if ((this.props.company !== null) && (this.props.company !== ''))  {
       company = this.props.company;
 
-      if ((this.props.address != null) && (this.props.address != '')) {
+      if ((this.props.address !== null) && (this.props.address !== '')) {
         company += ' ~ ' + this.props.address;
       }
     }
     else {
-      if ((this.props.address != null) && (this.props.address != '')) {
+      if ((this.props.address !== null) && (this.props.address !== '')) {
         company = this.props.address;
       }
     }
